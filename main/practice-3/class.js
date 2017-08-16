72415aa4ce34c2d04d93e63823e6fdd6a8126ca9
NxDon <645104928@qq.com>
2017-08-16 18:02:34 +0800

    finish all practice

--- main/practice-3/class.js
+++ main/practice-3/class.js
@@ -2,27 +2,34 @@ module.exports = class Class {
     constructor(number) {
         this.number = number;
         this.member = [];
-        this.teacher ='';
+        this.teacher = '';
     }
 
     assignLeader(student) {
-        if(this.hasStudent(student)){
+        if (this.teacher != false) {
+            if (this.hasStudent(student)) {
+                this.teacher.notifyLeaderAssigned(student);
+                this.leader = student.name;
+                return `Assign team leader successfully.`;
+            } else {
+                return `It is not one of us.`;
+            }
+        } else if (this.hasStudent(student)) {
             this.leader = student.name;
-            this.teacher.notifyLeaderAssigned(student);
             return `Assign team leader successfully.`;
-        }else{
+        } else {
             return `It is not one of us.`;
         }
     }
 
-    appendMember(student){
-        if(this.teacher != false){
+    appendMember(student) {
+        if (this.teacher != false) {
             this.teacher.notifyStudentAppended(student);
         }
         this.member.push(student.id);
     }
 
-    hasStudent(student){
+    hasStudent(student) {
         return this.member.includes(student.id);
     }
 };
