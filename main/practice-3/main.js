2bce80f9d06831c71f0f3af20798f96c12b93aef
NxDon <645104928@qq.com>
2017-08-17 17:33:47 +0800

    remove some irrelevant files

--- main/practice-3/main.js
+++
@@ -1,8 +0,0 @@
-const Teacher = require('./teacher');
-const Class = require("./class");
-const Student = require('./student');
-
-let clazz = new Class(2);
-let student = new Student("Jerry", 21, clazz);
-let teacher = new Teacher("Tom", 21, [clazz]);
-clazz.appendMember(student);
